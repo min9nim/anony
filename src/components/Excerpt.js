c3236bd0d2eba44bf35618d968350f1ec8d3ed63
Song, MinGu <min9nim@gmail.com>
2018-06-16 23:29:53 +0900

    List 디자인 변경

--- src/components/Excerpt.js
+++ src/components/Excerpt.js
@@ -20,9 +20,11 @@ export default class Excerpt extends React.Component {
             <div id={this.props.post.key} className="excerpt">
                 <div>
                     <div className="title h4"><Link to={"/post/" + this.props.post.key}>{this.props.post.title}</Link></div>
+                </div>
+                <div>
+                    <div className="meta" onClick={this.editPost}>{this.props.post.writer} - {moment(this.props.post.date).fromNow()}</div>
                     <PostMenu history={this.props.history} postKey={this.props.post.key}/>
                 </div>
-                <div className="meta" onClick={this.editPost}>{this.props.post.writer} - {moment(this.props.post.date).fromNow()}</div>
                 <div className="content">{this.props.post.content.substr(0,100)}</div>
             </div>
         );
