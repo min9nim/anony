c3cedd805d4b7ab7c6f0c6ae53dd4e21d0f0c6ef
songmingu(mac) <min9nim@gmail.com>
2018-07-17 11:34:06 +0900

    dbConfig.js 수정

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,5 @@
 const config = {
     dev : "mongodb://dev:dev123@ds239911.mlab.com:39911/dev",
-    prod : 'mongodb://dev:dev123@ds018538.mlab.com:18538/anony'
+    prod : 'mongodb://dev:dev123@ds239911.mlab.com:39911/anony'
 }
-
 module.exports = config;
