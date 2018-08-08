7f9a95aab8bd4366f88fe1fa3476cc2ba2b38811
songmingu(win) <min9nim@gmail.com>
2018-08-08 22:33:48 +0900

    검색결과 목록에서는 Menu 숨김 처리

--- src/pages/List.js
+++ src/pages/List.js
@@ -76,7 +76,8 @@ export default class List extends React.Component {
         return (
             <div className="list">
                 <div className="header">
-                    <div className="title" onClick={this.logoClick}>{title}</div><Menu/>
+                    <div className="title" onClick={this.logoClick}>{title}</div>
+                    {!search && <Menu/> }
                     <div className="status">{status}</div>
                     <Search context={this.props.context}/>
                 </div>
