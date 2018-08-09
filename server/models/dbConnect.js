42bc6402c15b7b04f94ce7571f8f37001941189a
songmingu(mac) <min9nim@gmail.com>
2018-08-09 15:39:08 +0900

    update

--- server/models/dbConnect.js
+++ server/models/dbConnect.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-
-//const dbConfig = process.env.PORT ? require("../../dbConfig") : require("../../../dbConfig");
-const dbConfig = require("../../../dbConfig");
-
 // 디비설정 
 const db = mongoose.connection;
 db.on('error', console.error);
@@ -17,9 +13,11 @@ console.log(process.argv);
 
 if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'development') {
     console.log("개발디비 접속");
+    const dbConfig = require("../../../dbConfig");
     mongoose.connect(dbConfig.dev);
   }else{
     console.log("운영디비 접속");
+    const dbConfig = require("../../dbConfig");
     mongoose.connect(dbConfig.prod);
 }
 
