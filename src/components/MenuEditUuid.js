385d8d119f0e9cc64df434ea0fc38ca95f4b6884
songmingu(mac) <min9nim@gmail.com>
2018-08-10 13:44:58 +0900

    uuid auto generate 기능 추가

--- src/components/MenuEditUuid.js
+++ src/components/MenuEditUuid.js
@@ -15,13 +15,13 @@ export default class MenuEditUuid extends React.Component {
         super(props);
         this.hideMenu = this.props.hideMenu;
         this.confirm = this.confirm.bind(this);
+        this.refreshUuid = this.refreshUuid.bind(this);
         this.cancel = this.cancel.bind(this);
         this.handleChange = this.handleChange.bind(this);
         
         this.state = {
             uuid: tp.user.uuid,
         }
-        //tp.shortid = shortid;
     }
 
 
@@ -41,17 +41,21 @@ export default class MenuEditUuid extends React.Component {
         this.hideMenu();
     }
 
+    refreshUuid(){
+        this.setState({uuid: shortid.generate()});
+    }
+
 
     getValidationState() {
         const length = this.state.uuid.length;
-        if (shortid.isValid(this.state.uuid) && this.state.uuid.length===9) return 'success';
+        if (shortid.isValid(this.state.uuid) && this.state.uuid.length >= 9) return 'success';
         else if (length > 5) return 'warning';
         else if (length > 0) return 'error';
         return null;
     }
 
     handleChange(e) {
-        if(e.target.value.length > 9) return;
+        if(e.target.value.length > 10) return;
         const state = {};
         state[e.target.id] = e.target.value ;
         this.setState(state);
@@ -63,14 +67,19 @@ export default class MenuEditUuid extends React.Component {
             <div className="uuid-setting">
                 <div className="modal_div"></div>
                 <div className="inner_div">
-                    <FormGroup className="form" controlId = "uuid" validationState = {this.getValidationState()}>
-                        {/* <ControlLabel> uuid </ControlLabel> */}
-                        <FormControl type = "text"
-                                value = {this.state.uuid}
-                                onChange = {this.handleChange}
-                                placeholder = "uuid.." />
-                        <FormControl.Feedback />
-                    </FormGroup>
+                    <div className="upper_grp">
+                        <FormGroup className="form" controlId = "uuid" validationState = {this.getValidationState()}>
+                            {/* <ControlLabel> uuid </ControlLabel> */}
+                            <FormControl type = "text"
+                                    autoFocus
+                                    value = {this.state.uuid}
+                                    onChange = {this.handleChange}
+                                    placeholder = "uuid.." />
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <div className="icon-spin3 refresh" onClick={this.refreshUuid} title="Gererage random uuid"/>
+                    </div>
+                    
                     <div className="btn_grp">
                         <Button className="searchBtn" bsStyle="success" onClick={this.confirm}>Save</Button>
                         <Button className="cancelBtn" bsStyle="success" onClick={this.cancel}>Cancel</Button>
