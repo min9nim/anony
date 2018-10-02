7e08d15437821027c93898394aaf7f50ec602974
songmingu(mac) <min9nim@gmail.com>
2018-10-03 00:51:23 +0900

    글보기화면에서 remove 할때도 애니메이션 추가

--- src/components/PostMenu.js
+++ src/components/PostMenu.js
@@ -52,10 +52,9 @@ export default class PostMenu extends React.Component {
 
 
     removePost(){
-        //if(!confirm("Remove this?")) return;
         tp.confirm({
-            //message: "Remove this?<br> you cannot restore this.", width: "212px",
-            message: "Remove this?", width: "155px",
+            message: "Remove this?", 
+            width: "155px",
             onYes: () => {                
                     tp.api.removePost({
                         key: this.props.postKey,
@@ -69,24 +68,37 @@ export default class PostMenu extends React.Component {
                             });
                             this.cancelMenu();
                         } else {
-                            document.getElementById(this.props.postKey).style.transform = "scaleY(0)";
-                            //post.style.height = "0px";
-                            setTimeout(() => {
-                                tp.store.dispatch(tp.action.removePost(p => p.key === this.props.postKey));
-                                if(this.props.postOrigin){
-                                    // 수정내역을 삭제할 경우
-                                    if(location.pathname.indexOf("postHistory") >= 0){
-                                        // postHistory 목록에서 삭제할 경우 화면 이동 없음
-                                    }else{
-                                        // 글보기 화면에서 삭제할 경우에는 히스토리 목록 화면으로 이동
+                            if(["PostHistory", "List"].includes(tp.thispage)){
+                                // 애니메이션 처리
+                                document.getElementById(this.props.postKey).style.transform = "scaleY(0)";
+
+                                // dom 제거
+                                setTimeout(() => {
+                                    tp.store.dispatch(tp.action.removePost(p => p.key === this.props.postKey));
+                                }, 500);
+
+                                // 목록에서 바로 삭제할 경우에는 화면이동 필요없음
+
+                            }else{
+                                // 애니메이션 처리
+                                document.getElementsByClassName("post")[0].style.transform = "scaleX(0)";
+
+                                // dom 제거
+                                setTimeout(() => {
+                                    tp.store.dispatch(tp.action.removePost(p => p.key === this.props.postKey));
+                                    
+                                    // 글보기 화면에서 삭제할 경우에는 목록화면으로 이동 필요
+                                    if(this.props.postOrigin){
                                         var arr = location.pathname.split("/");
                                         arr.splice(2, 2, "postHistory", this.props.postOrigin);     // context 명이 없는 경우 문제 발생할 수 있음
                                         this.props.history.push(arr.join("/"));
+                                    }else{
+                                        this.props.history.push(this.contextPath + "/list");
                                     }
-                                }else{
-                                    this.props.history.push(this.contextPath + "/list");
-                                }
-                            }, 500);
+
+                                }, 500);
+
+                            }
                         }
                     })                    
             }
