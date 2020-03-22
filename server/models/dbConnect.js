884bd6bc806029363ef6d9e1e22399f30679bc8e
min9nim <min9nim@gmail.com>
2020-03-22 16:17:42 +0900

    update

--- server/models/dbConnect.js
+++ server/models/dbConnect.js
@@ -14,8 +14,9 @@ const dburl = process.env.dburl
 console.log('dburl:', dburl)
 mongoose
   .connect(dburl)
-  .then(success => console.log(success))
+  .then(success => console.log('DB 연결 성공'))
   .catch(error => {
+    console.warn('디비 연결 실패..')
     console.log('dburl:', dburl)
     console.log(error)
   })
