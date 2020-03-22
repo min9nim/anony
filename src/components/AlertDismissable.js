72f50ff8add906845db6e8460a81059ed2c2ddab
min9nim <min9nim@gmail.com>
2020-03-23 05:12:59 +0900

    reformat

--- src/components/AlertDismissable.js
+++ src/components/AlertDismissable.js
@@ -1,74 +1,74 @@
-import React from "react";
-//import ReactDOM from 'react-dom';     // 이거보다 아래가 더 최적화를 위해 나은 코드일까? 18.08.19
-import {findDOMNode} from 'react-dom';
-
-import {
-    Alert,
-    Button
-} from "react-bootstrap";
-import "./AlertDismissable.scss";
-
+import React from 'react'
+import { findDOMNode } from 'react-dom'
+import { Alert, Button } from 'react-bootstrap'
+import './AlertDismissable.scss'
 
 export default class AlertDismissable extends React.Component {
-    constructor(props, context) {
-      super(props, context);
-  
-      this.handleDismiss = this.handleDismiss.bind(this);
-      this.handleShow = this.handleShow.bind(this);
-  
-      this.state = {
-        show: false,
-        style: "warning",   // success, warning, danger, info
-        message: "",
-        width: undefined,
-      };
+  constructor(props, context) {
+    super(props, context)
 
+    this.handleDismiss = this.handleDismiss.bind(this)
+    this.handleShow = this.handleShow.bind(this)
 
-      tp.view.AlertDismissable = this;
-    }
-  
-    handleDismiss() {
-      this.setState({ show: false, message: "" });
-      if(typeof this.onClose === "function"){
-        this.onClose();
-        this.onClose = undefined;
-      }
-    }
-  
-    handleShow({message, style="warning", width, onClose}) {
-      this.setState({
-        show: true,
-        message,
-        style,
-        width,
-      });
-      this.onClose = onClose;
+    this.state = {
+      show: false,
+      style: 'warning', // success, warning, danger, info
+      message: '',
+      width: undefined,
     }
 
-    componentDidUpdate(){
-      if(this.state.show)
-        //ReactDOM.findDOMNode(this.refs.closeBtn).focus();   // 이거보다 아래가 더 최적화를 위해 나은 코드일까? 18.08.19
-        findDOMNode(this.refs.closeBtn).focus();
-    }
+    tp.view.AlertDismissable = this
+  }
 
-  
-    render() {
-      if (this.state.show) {
-        return (
-          <div className="alertDismissable">
-            <div className="modal_div"></div>
-            <Alert bsStyle={this.state.style} style={{width: this.state.width}} onDismiss={this.handleDismiss}>
-              <p dangerouslySetInnerHTML={{__html: this.state.message}} />
-              <p>
-                {/* <Button bsStyle="danger">Take this action</Button>
+  handleDismiss() {
+    this.setState({ show: false, message: '' })
+    if (typeof this.onClose === 'function') {
+      this.onClose()
+      this.onClose = undefined
+    }
+  }
+
+  handleShow({ message, style = 'warning', width, onClose }) {
+    this.setState({
+      show: true,
+      message,
+      style,
+      width,
+    })
+    this.onClose = onClose
+  }
+
+  componentDidUpdate() {
+    if (this.state.show) findDOMNode(this.refs.closeBtn).focus()
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div className="alertDismissable">
+          <div className="modal_div"></div>
+          <Alert
+            bsStyle={this.state.style}
+            style={{ width: this.state.width }}
+            onDismiss={this.handleDismiss}
+          >
+            <p dangerouslySetInnerHTML={{ __html: this.state.message }} />
+            <p>
+              {/* <Button bsStyle="danger">Take this action</Button>
                 <span> or </span> */}
-                <Button bsStyle={this.state.style} ref="closeBtn" onClick={this.handleDismiss}>Close</Button>
-              </p>
-            </Alert>
-          </div>
-        );
-      }else{
-        return null;
-      }
+              <Button
+                bsStyle={this.state.style}
+                ref="closeBtn"
+                onClick={this.handleDismiss}
+              >
+                Close
+              </Button>
+            </p>
+          </Alert>
+        </div>
+      )
+    } else {
+      return null
     }
+  }
 }
