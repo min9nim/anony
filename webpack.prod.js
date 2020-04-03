174fe17cfe96021cc547758771241876ca0a7ca6
min9nim <min9nim@gmail.com>
2020-04-04 02:27:29 +0900

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
@@ -12,14 +12,14 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      reportFilename: 'docs/size_prod.html',
-      defaultSizes: 'parsed',
-      openAnalyzer: false,
-      generateStatsFile: false,
-      statsFilename: 'docs/stats_prod.json',
-    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    //   reportFilename: 'docs/size_prod.html',
+    //   defaultSizes: 'parsed',
+    //   openAnalyzer: false,
+    //   generateStatsFile: false,
+    //   statsFilename: 'docs/stats_prod.json',
+    // }),
   ],
 
   optimization: {
