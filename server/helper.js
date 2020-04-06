21abb85ab0ebeb5b561ef581126ea6853df0ae73
min9nim <min9nim@gmail.com>
2020-04-06 11:14:01 +0900

    add content-length to log

--- server/helper.js
+++ server/helper.js
@@ -1,11 +1,27 @@
 const moment = require('moment')
 const createLogger = require('if-logger').default
 
+function format(level, tags, message) {
+  const tagstr = tags
+    .map((tag) => (typeof tag === 'function' ? tag() : tag))
+    .join(' ')
+  return `${tagstr} ${message}`
+}
+
 function createContext(req, res, next) {
   const logger = createLogger({
+    format,
     tags: [currentTime, req.method, req.originalUrl],
   })
-  logger.verbose('context initialized')
+  logger.verbose.time('time:')
+  res.on('finish', () => {
+    logger.options.tags.push(res.statusCode)
+    const contentLength = res.getHeader('Content-Length')
+    if (contentLength) {
+      logger.options.tags.push(contentLength)
+    }
+    logger.verbose.timeEnd('time:')
+  })
   req.ctx = { logger }
   next()
 }
