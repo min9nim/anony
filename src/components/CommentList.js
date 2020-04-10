e19739908b30f26cff92ab609274da914a2bb7b7
min9nim <min9nim@gmail.com>
2020-04-10 10:32:10 +0900

    convert functional component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,54 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Comment } from '../components'
 import { ctx } from '@/biz/context'
 import './CommentList.scss'
 
-export class CommentList extends React.Component {
-  constructor(props) {
-    // ctx.logger.verbose("CommentList 생성자 호출");
-    super(props)
-    this.state = {
-      comments: ctx.store
+export function CommentList(props) {
+  ctx.logger.debug('CommentList render')
+
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    ctx.logger.debug('useEffect', comments.length)
+
+    const unsubscribe = ctx.store.subscribe(() => {
+      const newComments = ctx.store
         .getState()
-        .data.comments.filter(c => c.postKey === this.props.postKey),
-    }
-    ctx.view.CommentList = this
-
-    // 이후 CommentList 가 스토어 상태를 구독하도록 설정
-    this.unsubscribe = ctx.store.subscribe(() => {
-      // ctx.logger.verbose("# CommentList setState called..");
-      this.setState({
-        comments: ctx.store
-          .getState()
-          .data.comments.filter(c => c.postKey === this.props.postKey),
-      })
+        .data.comments.filter((c) => c.postKey === props.postKey)
+      ctx.logger.debug(
+        '스토어 변경감지하고 코멘츠 갱신처리',
+        newComments.length,
+      )
+      setComments(newComments)
     })
-
-    if (this.state.comments.length === 0 && this.props.commentCnt > 0) {
-      ctx.api
-        .getComments(this.props.postKey)
-        .then(res => ctx.store.dispatch(ctx.action.addComments(res.comments)))
+    if (comments.length === 0 && props.commentCnt > 0) {
+      ctx.logger.debug('load comments')
+      ctx.api.getComments(props.postKey).then((res) => {
+        ctx.logger.debug('loaded comments')
+        ctx.store.dispatch(ctx.action.addComments(res.comments))
+        ctx.logger.debug('스토어 dispatch')
+        const commentList = ctx.store
+          .getState()
+          .data.comments.filter((c) => c.postKey === props.postKey)
+        setComments(commentList)
+      })
     }
-  }
 
-  componentWillUnmount() {
-    // ctx.logger.verbose("# CommentList unsubscribe store..");
-    this.unsubscribe()
-  }
-
-  render() {
-    //ctx.logger.verbose("CommentList 렌더링..");
-    return (
-      <div className="CommentList">
-        {this.state.comments.map(comment => (
-          <Comment
-            history={this.props.history}
-            key={comment.key}
-            comment={comment}
-            context={this.props.context}
-          />
-        ))}
-      </div>
-    )
-  }
+    return () => {
+      ctx.logger.debug('unsubscribe')
+      unsubscribe()
+    }
+  }, [comments])
+  return (
+    <div className="CommentList">
+      {comments.map((comment) => (
+        <Comment
+          history={props.history}
+          key={comment.key}
+          comment={comment}
+          context={props.context}
+        />
+      ))}
+    </div>
+  )
 }
