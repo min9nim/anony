ed637442a395eea1ea5c309bbefc1fc16fac396d
min9nim <min9nim@gmail.com>
2020-04-10 12:38:39 +0900

    refactor

--- src/components/MyChannels.js
+++ src/components/MyChannels.js
@@ -3,39 +3,39 @@ import $m from '../../com/util'
 import { Loading } from './Loading'
 import './MyChannels.scss'
 
-export const MyChannels = props => {
+function handleClick(channel) {
+  ctx.context = channel
+  ctx.api
+    .getPosts({ idx: 0, cnt: 10, context: channel })
+    .then((res) => ctx.store.dispatch(ctx.action.setPosts(res.posts)))
+    .then(() => {
+      ctx.isScrollLast = false
+    })
+    .then(toggleChannels)
+    .then(() => {
+      ctx.history.push('/' + channel)
+    })
+}
+
+function toggleChannels() {
+  if ($m('.my-channels').css('display') === 'block') {
+    $m('.my-channels').hide()
+  } else {
+    $m('.my-channels').show()
+  }
+}
+
+export const MyChannels = (props) => {
   const { channels } = ctx.store.getState().data
   useEffect(() => {
     if (channels.length > 0) {
       return
     }
     // 로드된 채널 정보가 없다면 api 호출
-    ctx.api.myChannels().then(res => {
+    ctx.api.myChannels().then((res) => {
       ctx.store.dispatch(ctx.action.myChannels(res.output))
     })
   })
-
-  function handleClick(channel) {
-    ctx.context = channel
-    ctx.api
-      .getPosts({ idx: 0, cnt: 10, context: channel })
-      .then(res => ctx.store.dispatch(ctx.action.setPosts(res.posts)))
-      .then(() => {
-        ctx.isScrollLast = false
-      })
-      .then(toggleChannels)
-      .then(() => {
-        ctx.history.push('/' + channel)
-      })
-  }
-
-  function toggleChannels() {
-    if ($m('.my-channels').css('display') === 'block') {
-      $m('.my-channels').hide()
-    } else {
-      $m('.my-channels').show()
-    }
-  }
 
   return (
     <div className="my-channels">
@@ -43,7 +43,7 @@ export const MyChannels = props => {
       {channels.length === 0 ? (
         <Loading className="loading2" name="list" />
       ) : (
-        channels.map(item => {
+        channels.map((item) => {
           return (
             <div
               key={item.name}
