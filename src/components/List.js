834d1b2102b0df3befa721844938fa45d5b60a91
min9nim <min9nim@gmail.com>
2020-04-23 10:10:49 +0900

    [refactor] 이펙트 함수 분리

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,5 @@
 import { withLogger } from '@/biz'
 import { ctx } from '@/biz/context'
-import { observeDom } from '@/biz/utils'
 import {
   Excerpt,
   ListLoader,
@@ -13,9 +12,8 @@ import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { setPosts, setSearch, addPosts, setPostsAsync } from '@/redux/action'
 import { connect } from 'react-redux'
-import { prop, isNil, pipe, last } from 'ramda'
-import { needToFetch, fetchList } from './List-fn'
-
+import { prop, isNil, pipe } from 'ramda'
+import { initializeEffect, infiniteLoadingEffect } from './List-fn'
 import './List.scss'
 
 function List(props) {
@@ -25,7 +23,11 @@ function List(props) {
     loading: false,
   })
 
-  const logoClick = async () => {
+  const setLoading = (loading) => {
+    setState({ ...state, loading })
+  }
+
+  const logoClick = () => {
     // 기존내용 초기화
     props.setSearch('')
     ctx.noMore = false
@@ -41,71 +43,16 @@ function List(props) {
   const posts = props.state.data.posts.filter(pipe(prop('origin'), isNil))
 
   // initialize
-  useEffect(() => {
-    props.logger.verbose('[effect-in] initialize')
-    ctx.scrollTop = 0 // 스크롤 위치 초기화
-    document.title = (ctx.context || 'Anony') + ' - ' + ctx.thispage
-
-    if (props.context && props.context.length > ctx.MAXCONTEXTLEN) {
-      alert(`채널이름은 최대 ${ctx.MAXCONTEXTLEN}자 까지 가능합니다`)
-      history.back()
-      return
-    }
-
-    if (needToFetch(props)) {
-      props.setPostsAsync({ idx: 0, cnt: 10, context: ctx.context })
-    }
-  }, [])
+  useEffect(initializeEffect(props), [])
 
   // infinite loading
-  useEffect(() => {
-    props.logger.verbose('[effect-in] infinite loading')
-    ctx.$m.scrollTo(0, ctx.scrollTop) // 이전 스크롤 위치로 복원
-
-    const lastPost = last(document.querySelectorAll('.list > .excerpt'))
-
-    if (!lastPost) {
-      props.logger.warn('not found lastPost')
-      return () => {}
-    }
-    if (ctx.noMore) {
-      props.logger.info('no more data')
-      return
-    }
-    props.logger.info('observe last one')
-
-    const unobserve = observeDom(
-      lastPost,
-      fetchList(props, posts, state, setState),
-    )
-    return () => {
-      props.logger.verbose('[effect-out] infinite loading')
-
-      // 현재 목록화면 scrollTop 의 값
-      ctx.scrollTop = Math.max(
-        document.documentElement.scrollTop,
-        document.body.scrollTop,
-      )
-
-      if (lastPost.observed) {
-        props.logger.info('unobserve last one')
-        unobserve()
-      }
-    }
-  }, [posts.length])
+  useEffect(infiniteLoadingEffect(props, setLoading), [posts.length])
 
   props.logger.verbose('render', state.loading)
-  //let title = props.state.view.uuid + (ctx.context ? (" /" + ctx.context) : "") ;
-  let title = ctx.user.uuid + (ctx.context ? ' /' + ctx.context : '')
   let uuid = ctx.user.uuid
   let channel = ctx.context ? ' /' + ctx.context : ''
-
-  let status = ''
   let search = props.state.view.search
-
-  if (search) {
-    status = ` > ${search}'s result`
-  }
+  let status = search ? (status = ` > ${search}'s result`) : ''
 
   return (
     <div className="list">
@@ -114,9 +61,6 @@ function List(props) {
           <img src="/image/logo_transparent.png" onClick={logoClick} />
         </div>
         <Search context={ctx.context} />
-
-        {/* <div className="status">{status}</div> */}
-
         <div className="menu-title">
           {/* <Menu /> */}
           <div
