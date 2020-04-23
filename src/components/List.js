6fe7c638b6dd99f62eba7ed3bdd7b88305d0de02
min9nim <min9nim@gmail.com>
2020-04-23 10:38:56 +0900

    [refactor] smart & dumb design

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,12 @@
 import { withLogger } from '@/biz'
 import { ctx } from '@/biz/context'
-import {
-  Excerpt,
-  ListLoader,
-  MenuBoard,
-  MyChannels,
-  Search,
-} from '@/components'
 import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import { setPosts, setSearch, addPosts, setPostsAsync } from '@/redux/action'
 import { connect } from 'react-redux'
 import { prop, isNil, pipe } from 'ramda'
 import { initializeEffect, infiniteLoadingEffect } from './List-fn'
 import './List.scss'
+import ListTemplate from './ListTemplate'
 
 function List(props) {
   const [state, setState] = useState({
@@ -23,9 +15,8 @@ function List(props) {
     loading: false,
   })
 
-  const setLoading = (loading) => {
-    setState({ ...state, loading })
-  }
+  const setLoading = (loading) => setState({ ...state, loading })
+  const setMenuClicked = (menuClicked) => setState({ ...state, menuClicked })
 
   const logoClick = () => {
     // 기존내용 초기화
@@ -39,7 +30,6 @@ function List(props) {
       context: ctx.context,
     })
   }
-
   const posts = props.state.data.posts.filter(pipe(prop('origin'), isNil))
 
   // initialize
@@ -48,72 +38,15 @@ function List(props) {
   // infinite loading
   useEffect(infiniteLoadingEffect(props, setLoading), [posts.length])
 
-  props.logger.verbose('render', state.loading)
-  let uuid = ctx.user.uuid
-  let channel = ctx.context ? ' /' + ctx.context : ''
-  let search = props.state.view.search
-  let status = search ? (status = ` > ${search}'s result`) : ''
-
   return (
-    <div className="list">
-      <div className="header">
-        <div className="logo">
-          <img src="/image/logo_transparent.png" onClick={logoClick} />
-        </div>
-        <Search
-          context={ctx.context}
-          setLoading={setLoading}
-          loading={state.loading}
-        />
-        <div className="menu-title">
-          {/* <Menu /> */}
-          <div
-            className="icon-menu-1 menu"
-            onClick={() => setState({ ...state, menuClicked: true })}
-          ></div>
-          <div className="uuid">{uuid}</div>
-        </div>
-        <div className="channel">{channel}</div>
-      </div>
-
-      {posts.map((post) => (
-        <Excerpt
-          history={props.history}
-          key={post.key}
-          post={post}
-          context={ctx.context}
-        />
-      ))}
-
-      {state.loading && <ListLoader />}
-
-      {props.state.view.search !== '' && (
-        <div className="backBtn">
-          <Button bsStyle="success" onClick={logoClick}>
-            Back
-          </Button>
-        </div>
-      )}
-
-      <div className="writeBtn">
-        <Link to={'/' + ctx.context + '/write'}>
-          <Button bsStyle="success">
-            <i className="icon-doc-new" />
-            Write
-          </Button>
-        </Link>
-      </div>
-
-      <div className="channels-wrappter">
-        <MyChannels />
-      </div>
-
-      {state.menuClicked && (
-        <MenuBoard
-          hideMenu={() => setState({ ...state, menuClicked: false })}
-        />
-      )}
-    </div>
+    <ListTemplate
+      logoClick={logoClick}
+      setMenuClicked={setMenuClicked}
+      posts={posts}
+      setLoading={setLoading}
+      loading={state.loading}
+      menuClicked={state.menuClicked}
+    />
   )
 }
 
---
+++ src/components/ListTemplate.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {
+  Excerpt,
+  ListLoader,
+  MenuBoard,
+  MyChannels,
+  Search,
+} from '@/components'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default function ListTemplate({
+  logoClick,
+  setMenuClicked,
+  posts,
+  setLoading,
+  loading,
+  menuClicked,
+}) {
+  const channel = ctx.context ? ' /' + ctx.context : ''
+  return (
+    <div className="list">
+      <div className="header">
+        <div className="logo">
+          <img src="/image/logo_transparent.png" onClick={logoClick} />
+        </div>
+        <Search
+          context={ctx.context}
+          setLoading={setLoading}
+          loading={loading}
+        />
+        <div className="menu-title">
+          {/* <Menu /> */}
+          <div
+            className="icon-menu-1 menu"
+            onClick={() => setMenuClicked(true)}
+          ></div>
+          <div className="uuid">{ctx.user.uuid}</div>
+        </div>
+        <div className="channel">{channel}</div>
+      </div>
+
+      {posts.map((post) => (
+        <Excerpt
+          history={ctx.history}
+          key={post.key}
+          post={post}
+          context={ctx.context}
+        />
+      ))}
+
+      {loading && <ListLoader />}
+
+      {ctx.store.getState().view.search !== '' && (
+        <div className="backBtn">
+          <Button bsStyle="success" onClick={logoClick}>
+            Back
+          </Button>
+        </div>
+      )}
+
+      <div className="writeBtn">
+        <Link to={'/' + ctx.context + '/write'}>
+          <Button bsStyle="success">
+            <i className="icon-doc-new" />
+            Write
+          </Button>
+        </Link>
+      </div>
+
+      <div className="channels-wrappter">
+        <MyChannels />
+      </div>
+
+      {menuClicked && <MenuBoard hideMenu={() => setMenuClicked(false)} />}
+    </div>
+  )
+}
