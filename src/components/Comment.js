fd09541396addce1fde03ccf788cd2704960435a
min9nim <min9nim@gmail.com>
2020-04-23 09:17:33 +0900

    [refactor] remove this.state

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -6,38 +6,28 @@ import './Comment.scss'
 
 export class Comment extends React.Component {
   constructor(props) {
-    // ctx.logger.verbose("Comment 생성자 호출");
     super(props)
-    this.state = {
-      key: '',
-      writer: '',
-      content: '',
-      date: '',
-      uuid: '',
-      postKey: '',
-    }
   }
 
   render() {
     // ctx.logger.verbose("Comment 렌더링");
-    if (this.props.comment) {
-      // Comment 프롭이 들어오는 경우는 다시 업데이트하지 말라고 일부러 setState 를 사용하지 않고 state를 갱신함
-      this.state = this.props.comment
-    }
-    const html = ctx.$m.txtToHtml(this.state.content)
-
+    // if (this.props.comment) {
+    //   // Comment 프롭이 들어오는 경우는 다시 업데이트하지 말라고 일부러 setState 를 사용하지 않고 state를 갱신함
+    //   this.state = this.props.comment
+    // }
+    const { comment } = this.props
+    const html = ctx.$m.txtToHtml(comment.content)
     return (
       <div className="comment">
         <div>
           <div className="meta">
-            {this.state.writer} -{' '}
-            {moment(this.state.date).format('MM/DD dd HH:mm')}
+            {comment.writer} - {moment(comment.date).format('MM/DD dd HH:mm')}
           </div>
-          <CommentMenu comment={this.state} />
+          <CommentMenu comment={comment} />
         </div>
         {/*댓글에서 새줄표시 <br> 처리하기 위해 html을 사용할 수 있어야 함*/}
         <div
-          className={this.state.deleted ? 'content deleted' : 'content'}
+          className={comment.deleted ? 'content deleted' : 'content'}
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
