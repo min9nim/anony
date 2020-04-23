b3fee91c1d6e8c40e8c7748315e735c37d14accf
min9nim <min9nim@gmail.com>
2020-04-23 12:03:07 +0900

    [refactor] use saga and template

--- src/components/PostMenuTemplate.js
+++ src/components/PostMenuTemplate.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import './PostMenuTemplate.scss'
 
 export default function PostMenuTemplate(props) {
   const {
--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,5 +1,9 @@
-import { type, updatePost } from '@/redux/action'
-import { call, put, takeEvery } from 'redux-saga/effects'
+import {
+  type,
+  updatePost,
+  removePost as removePostAction,
+} from '@/redux/action'
+import { call, put, takeEvery, delay } from 'redux-saga/effects'
 
 const {
   DELETEPOST,
@@ -7,6 +11,9 @@ const {
   DELETEPOST_FAILED,
   LIKEPOST_REQUESTED,
   LIKEPOST_FAILED,
+  REMOVEPOST_FAILED,
+  REMOVEPOST_REQUESTED,
+  REMOVEPOST,
 } = type
 
 function* deletePost(action) {
@@ -39,7 +46,46 @@ function* likePost(action) {
   }
 }
 
+function* removePost(action) {
+  try {
+    yield call(ctx.api.removePost, {
+      key: action.key,
+      uuid: ctx.user.uuid,
+    })
+
+    if (['PostHistory', 'List'].includes(ctx.thispage)) {
+      yield call(() => {
+        document.getElementById(action.key).style.transform = 'scaleY(0)'
+      })
+      yield delay(500)
+      yield put(removePostAction((p) => p.key === action.key))
+    } else {
+      yield call(() => {
+        document.getElementsByClassName('post')[0].style.transform = 'scaleX(0)'
+      })
+      yield delay(500)
+      yield put(removePostAction((p) => p.key === action.key))
+      yield call(() => {
+        if (action.postOrigin) {
+          var arr = location.pathname.split('/')
+          arr.splice(2, 2, 'postHistory', action.postOrigin) // context 명이 없는 경우 문제 발생할 수 있음
+          ctx.history.push(arr.join('/'))
+        } else {
+          ctx.history.push(action.contextPath + '/list')
+        }
+      })
+    }
+  } catch (e) {
+    yield put({ type: REMOVEPOST_FAILED, message: e.message })
+    ctx.alert({
+      message: e.message,
+      style: 'warning',
+    })
+  }
+}
+
 export default function* () {
   yield takeEvery(DELETEPOST_REQUESTED, deletePost)
   yield takeEvery(LIKEPOST_REQUESTED, likePost)
+  yield takeEvery(REMOVEPOST_REQUESTED, removePost)
 }
