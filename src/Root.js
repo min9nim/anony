024a7e372ba4d21b16080f7f4c88268640b8160b
min9nim <min9nim@gmail.com>
2020-04-29 16:47:03 +0900

    다크모드일때 theme-color 변경

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
 import useDarkMode from 'use-dark-mode'
 
+function getMeta(metaName) {
+  const metas = document.getElementsByTagName('meta')
+  for (let i = 0; i < metas.length; i++) {
+    if (metas[i].getAttribute('name') === metaName) {
+      return metas[i]
+    }
+  }
+  return ''
+}
+
 const Root = () => {
   const { value } = useDarkMode(false)
-  ctx.logger.info('dark value', value)
+  ctx.logger.info('[Root render] dark value', value)
+  useEffect(() => {
+    ctx.logger.info('[Root render] effect')
+    if (value) {
+      const meta = getMeta('theme-color')
+      meta.setAttribute('content', '#10171d')
+    }
+  })
+
   return (
     <BrowserRouter>
       <Route path="/" component={App} />
